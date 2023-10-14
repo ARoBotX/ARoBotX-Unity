4a0157881cb962ca12f99b3d8e74d68e2f68abf7
Tharindu Gimras <75577550+Hedes-Productions@users.noreply.github.com>
2023-10-15 01:43:05 +0530

    Add robot movement with events

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -1,115 +1,85 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing.Text;
 using UnityEngine;
-using Vuforia;
+using UnityEngine.Events;
 
 public class RobotController : MonoBehaviour
 {
-    [SerializeField] private GameObject _robotPrefab;
-    [SerializeField] private PlaneFinderBehaviour _planeFinder;
-    [SerializeField] private Transform _arCamera;
-    [SerializeField] private GameObject _groundPrefab;
-    [SerializeField] private float _xOffset;
-    [SerializeField] private float _zOffset;
+    public static UnityEvent OnForwardClicked;
+    [HideInInspector] public static UnityEvent OnLeftClicked;
+    [HideInInspector] public static UnityEvent OnRightClicked;
+    [HideInInspector] public static UnityEvent OnBackwardClicked;
 
-    [SerializeField] private bool _testingModeOn;
-    private GameObject _tempCreatedRobot;
+    private GameObject _robot;
 
-    private bool _isCreated = false;
-    private GameObject _ground;
-    private float realLength = 200f;
-    private float realBreadth = 300f;
+    private void OnDisable()
+    {
+        OnForwardClicked.RemoveListener(GoForward);
+        OnLeftClicked.RemoveListener(GoLeft);
+        OnRightClicked.RemoveListener(GoRight);
+        OnBackwardClicked.RemoveListener(GoBackWard);
+    }
+    void FindRobot()
+    {
+        if (_robot == null)
+        {
+            _robot = GameObject.FindWithTag("Robot");
+        }
+        
+    }
 
+    void GoForward()
+    {
+        FindRobot();
+        Transform robotAttributes = _robot.transform;
+        robotAttributes.position += Vector3.forward * Time.deltaTime;
+    }
+
+    void GoLeft()
+    {
+        FindRobot();
+        Transform robotAttributes = _robot.transform;
+        robotAttributes.position += Vector3.left * Time.deltaTime;
+
+    }
+
+    void GoRight()
+    {
+        FindRobot();
+        Transform robotAttributes = _robot.transform;
+        robotAttributes.position += Vector3.right * Time.deltaTime;
+
+    }
+
+    void GoBackWard()
+    {
+        FindRobot();
+        Transform robotAttributes = _robot.transform;
+        robotAttributes.position += Vector3.back * Time.deltaTime;
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (OnForwardClicked == null)
+            OnForwardClicked = new UnityEvent();
+        if (OnLeftClicked == null)
+            OnLeftClicked = new UnityEvent();
+        if (OnRightClicked == null)
+            OnRightClicked = new UnityEvent();
+        if (OnBackwardClicked == null)
+            OnBackwardClicked = new UnityEvent();
+
+        OnForwardClicked.AddListener(GoForward);
+        OnLeftClicked.AddListener(GoLeft);
+        OnRightClicked.AddListener(GoRight);
+        OnBackwardClicked.AddListener(GoBackWard);
     }
 
     // Update is called once per frame
     void Update()
     {
-#if UNITY_EDITOR
-        if (!_isCreated) 
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Debug.Log("Working1");
-                _isCreated = true;
-                CreateMapRobotSetup();
-                
-            }
-            
-        }
-
-        if (_testingModeOn && _isCreated)
-        {
-            Vector3 robotVirtualPosition = ActualPositionsMapper(0, 0);
-            Debug.Log(robotVirtualPosition);
-            Transform robotSpawnPoint = _ground.transform.Find("RobotSpwanPoint");
-            _tempCreatedRobot.transform.position = robotVirtualPosition;
-            _tempCreatedRobot.transform.rotation = robotSpawnPoint.rotation;
-            Debug.Log("Testing mode on");
-        }
-#endif
-    }
-
-    private void CreateMapRobotSetup() 
-    {
-        StartCoroutine(InitialRobotPlacement(0,0));
-    }
-
-
-    private void InstantiateGround()
-    {
-        Transform planeFinderAttributes = _planeFinder.PlaneIndicator.transform;
-        Transform arCameraAttributes = _arCamera.transform;
-        Vector3 cameraRobotLookingDirection = planeFinderAttributes.position - arCameraAttributes.position;
-        cameraRobotLookingDirection.y = 0;
-        Quaternion cameraRobotLookingDirectionQ = Quaternion.LookRotation(-cameraRobotLookingDirection, Vector3.up);
-        _ground = Instantiate(_groundPrefab, planeFinderAttributes.position, cameraRobotLookingDirectionQ);
-    }
-
-    private void InstantiateRobot(Vector3 robotVirtualPosition)
-    {
-        Transform robotSpawnPoint = _ground.transform.Find("RobotSpwanPoint");
-        if (_testingModeOn)
-        {
-            _tempCreatedRobot = Instantiate(_robotPrefab, robotVirtualPosition, robotSpawnPoint.rotation);
-        }
-        else
-        {
-            GameObject robot = Instantiate(_robotPrefab, robotVirtualPosition, robotSpawnPoint.rotation);
-        }
-    }
-    private IEnumerator InitialRobotPlacement(float robotActualPositionX, float robotActualPositionY) 
-    {
         
-        InstantiateGround();
-        Vector3 robotVirtualPosition = ActualPositionsMapper(robotActualPositionX, robotActualPositionY);
-        InstantiateRobot(robotVirtualPosition);
-        //GameObject robotModelClone = Instantiate(_robotPrefab, planeAttributes.position, cameraRobotLookingDirectionQ); 
-        yield return new WaitForSeconds(1);
-    }
-
-    // Maps X,Y coodinates like this
-    // X => X to Virtual X , there is no real height to virtual constant Y , Y to Virtual Z
-    private Vector3 ActualPositionsMapper(float robotActualPositionX, float robotActualPositionY)
-    {
-        MeshRenderer robotPrefabMesh = _robotPrefab.GetComponent<MeshRenderer>();
-        float lengthScale = 10 / realLength;
-        float breadthScale = 10 / realBreadth;
-        Transform robotSpawnPoint = _ground.transform.Find("RobotSpwanPoint");
-        if (!_testingModeOn)
-        {
-            _xOffset = 2.5f;
-            _zOffset = 2.5f;
-        }
-        Vector3 robotVirtualPosition = new Vector3(robotActualPositionX * lengthScale - _xOffset, robotSpawnPoint.position.y + robotPrefabMesh.bounds.size.y / 2, robotActualPositionY * breadthScale - _zOffset);
-        return robotVirtualPosition;
     }
 }
